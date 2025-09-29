da2d8ff3fc42b3025cb279b27c19bd07bdfe72dc
M. Imam Firdaus <mimamfirdaus5@gmail.com>
2025-09-29 14:04:06 +0700

    add truncane

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,25 +3,30 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-    public function run(): void
+    public function run()
     {
+        // Matikan foreign key sementara
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         User::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
+        // Tambahkan user default
         User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password'),
+            'name' => 'Admin',
+            'email' => 'admin@codealpha.com',
+            'password' => Hash::make('password123'),
         ]);
 
         User::create([
-            'name' => 'Test User',
-            'email' => 'user@example.com',
-            'password' => Hash::make('password'),
+            'name' => 'Player One',
+            'email' => 'player1@codealpha.com',
+            'password' => Hash::make('password123'),
         ]);
     }
 }
