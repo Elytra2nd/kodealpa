7334ee448fb67ea60acc796aba4f913828c51918
M. Imam Firdaus <mimamfirdaus5@gmail.com>
2025-10-09 22:40:05 +0700

    fix fix

--- resources/js/Components/Grimoire/GrimoirePanel.tsx
+++ resources/js/Components/Grimoire/GrimoirePanel.tsx
@@ -10,7 +10,7 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = '/js/pdf.worker.min.js';
 
 interface User {
   id: number;
